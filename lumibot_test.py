e19244e84323e8fa645a899b6731ff0b103826e2
bryanli200282 <bryanli200282@gmail.com>
2023-12-28 23:48:26 -0500

    updated the strat

--- lumibot_test.py
+++ lumibot_test.py
@@ -1,138 +1,82 @@
-from alpaca.trading.client import TradingClient
-from alpaca.data.historical import StockHistoricalDataClient
-from alpaca.data.historical import StockHistoricalDataClient
-from alpaca.data.requests import StockLatestQuoteRequest
-from alpaca.trading.requests import GetAssetsRequest
-from alpaca.trading.enums import AssetClass
-from alpaca.data.requests import StockBarsRequest
-from alpaca.data.timeframe import TimeFrame
-import requests
-from datetime import datetime
-import config
-import pandas as pd
-import numpy as np
-import plotly
-import plotly.graph_objects as go
-import matplotlib.pyplot as plt
-from plotly.subplots import make_subplots
-import alpaca_trade_api as tradeapi
-import pandas as pd
-from TA import *
 from config import ALPACA_CONFIG
+from datetime import datetime
 from lumibot.backtesting import YahooDataBacktesting
 from lumibot.brokers import Alpaca
-from lumibot.strategies.strategy import Strategy
+from lumibot.strategies import Strategy
 from lumibot.traders import Trader
+import numpy as np
+import pandas as pd
 
 
-trading_client = StockHistoricalDataClient(config.API_KEY, config.SECRET)
+class MACDStrategy(Strategy):
 
-request_params = StockBarsRequest(
-                        symbol_or_symbols=["SPY"],
-                        timeframe=TimeFrame.Day,
-                        start="2022-01-01 00:00:00"
+    def initialize(self):
+        signal = None
+        start = "2022-01-01"
 
-                 )
-bars = trading_client.get_stock_bars(request_params)
-df = bars.df
-
-df.reset_index(inplace=True)
-
-
-fig = make_subplots(rows=2, cols=1)
-
-# Create candlestick trace
-candlestick = go.Candlestick(
-    x=df['timestamp'],
-    open=df['open'],
-    high=df['high'],
-    low=df['low'],
-    close=df['close'],
-    name='Candlestick'
-)
-
-# Calculate SMAs
-# Instantiate the SMA class for 20 and 50 periods
-sma_20 = SimpleMovingAverages(df, [20])
-sma_20.run()
-sma_20_values = sma_20.get_series(20)
-
-sma_50 = SimpleMovingAverages(df, [50])
-sma_50.run()
-sma_50_values = sma_50.get_series(50)
-
-# MACD calculation
-macd_indicator = MACD(df, 12, 26, 9)
-macd_indicator.calculate_macd()
-macd_line = macd_indicator.get_macd_line()
-signal_line = macd_indicator.get_signal_line()
-macd_histogram = macd_indicator.get_macd_histogram()
-
-
-class MyStrategy(Strategy):
-    # Custom parameters
-    parameters = {
-        "symbol": "SPY",
-        "quantity": 75,
-        "side": "buy"
-    }
-
-    def initialize(self, symbol=""):
-        # Will make on_trading_iteration() run every 180 minutes
+        self.signal = signal
+        self.start = start
         self.sleeptime = "1D"
 
     def on_trading_iteration(self):
-        symbol = self.parameters["symbol"]
-        quantity = self.parameters["quantity"]
-        side = self.parameters["side"]
-
-        bars = self.get_historical_prices("SPY", 100, "day")
+        bars = self.get_historical_prices("SPY", 27, "day")
         df = bars.df
 
-        # Then, to get the DataFrame of SPY data
-        df = bars.df
-        # Calculate MACD values
         short_period = 12
         long_period = 26
         signal_period = 9
 
-        # Calculate Short and Long EMAs
         df['ShortEMA'] = df['close'].ewm(span=short_period, adjust=False).mean()
         df['LongEMA'] = df['close'].ewm(span=long_period, adjust=False).mean()
-        df['LongEMA'] = df['close'].ewm(span=200, adjust=False).mean()
-
-        # Calculate MACD line
         df['MACD'] = df['ShortEMA'] - df['LongEMA']
-
-        # Calculate Signal line (9-day EMA of MACD)
         df['Signal_Line'] = df['MACD'].ewm(span=signal_period, adjust=False).mean()
 
-        # Calculate MACD Histogram
-        df['MACD_Histogram'] = df['MACD'] - df['Signal_Line']
+        df['Signal'] = np.where(
+            (df['MACD'] > df['Signal_Line']) & (df['MACD'].shift(1) < df['Signal_Line'].shift(1)),
+            "BUY", None
+        )
+        df['Signal'] = np.where(
+            (df['MACD'] < df['Signal_Line']) & (df['MACD'].shift(1) > df['Signal_Line'].shift(1)),
+            "SELL", df['Signal']
+        )
 
-        # Check for crossover - Buy when MACD crosses above Signal line
-        if df['Signal_Line'].iloc[-1] > df['MACD'].iloc[-1]:
-            # Generate buy signal
+        self.signal = df.iloc[-1].Signal
+
+        symbol = "SPY"  # Change symbol if needed
+        quantity = 1000  # Change quantity if needed
+
+        if self.signal == 'BUY':
+            pos = self.get_position(symbol)
+            if pos is not None:
+                self.sell_all()
+                
             order = self.create_order(symbol, quantity, "buy")
             self.submit_order(order)
-        # Check for crossover - Sell when MACD crosses below Signal line
-        elif df['Signal_Line'].iloc[-1] < df['MACD'].iloc[-1]:
-            # Generate sell signal
+        
+
+        elif self.signal == 'SELL':
+            pos = self.get_position(symbol)
+            if pos is not None:
+                self.sell_all()
+                
             order = self.create_order(symbol, quantity, "sell")
             self.submit_order(order)
+
+
+    
 
 if __name__ == "__main__":
     trade = False
     if trade:
         broker = Alpaca(ALPACA_CONFIG)
-        strategy = MyStrategy(broker=broker)
-        trader = Trader()
-        trader.add_strategy(strategy)
-        trader.run_all()
+        strategy = MACDStrategy(broker=broker)
+        bot = Trader()
+        bot.add_strategy(strategy)
+        bot.run_all()
     else:
-        start = datetime(2022, 1, 1)
-        end = datetime(2023, 10, 31)
-        MyStrategy.backtest(
+        start = datetime(2023, 1, 15)
+        end = datetime(2023, 12, 15)
+        MACDStrategy.backtest(
             YahooDataBacktesting,
             start,
             end
